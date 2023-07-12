6c5a33d72316085e7c29d45561118e37a6b8845e
Alexander Mamlygo <alexander.mamlygo@gmail.com>
2023-07-12 12:50:47 +0400

    Initial Commit

---
+++ TrafficLight_SwiftUI/TrafficLight_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLight_SwiftUIApp.swift
+//  TrafficLight_SwiftUI
+//
+//  Created by Aleksandr Mamlygo on 12.07.23.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLight_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
